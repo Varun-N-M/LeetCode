71e9dce8aad8c2211ea616fcef9edb28028fc3d8
Varun-N-M <114069624+Varun-N-M@users.noreply.github.com>
2023-09-21 17:00:25 +0530

    Time: 51 ms (61.92%), Space: 41.7 MB (76.47%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let filteredArr = []
+    for(let i=0;i<arr.length;i++){
+        fn(arr[i], i) ? filteredArr.push(arr[i]):""
+    }
+    return filteredArr
+};
