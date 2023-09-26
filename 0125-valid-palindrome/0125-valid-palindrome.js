1f665cba34ecf08bbcaf32885a45a49dbfe214a2
Varun-N-M <114069624+Varun-N-M@users.noreply.github.com>
2023-09-26 14:54:26 +0530

    Time: 58 ms (79.69%), Space: 45.4 MB (62.01%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,15 +3,9 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-//     let clean = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
+    let clean = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
     
-//     let reverse = clean.split(" ").reverse().join("")
-//     return reverse === clean
-    const cleanStr = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-  
-  // Reverse the cleaned string
-  const reversedStr = cleanStr.split('').reverse().join('');
-  
-  // Check if the cleaned string is the same as its reverse
-  return cleanStr === reversedStr;
+    let reverse = clean.split("").reverse().join("")
+    return reverse === clean
+
 };
