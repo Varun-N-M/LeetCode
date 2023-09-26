bb23b3522be0d0ec8732722d79770380306eb77b
Varun-N-M <114069624+Varun-N-M@users.noreply.github.com>
2023-09-26 14:24:01 +0530

    Time: 76 ms (51.25%), Space: 41.7 MB (96.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,14 +3,22 @@
  * @param {number} target
  * @return {number[]}
  */
+
 var twoSum = function(nums, target) {
-    const newMap = [];
-    for(let i = 0; i<nums.length; i++){
-        const currentNum = nums[i]
-        const required = target - currentNum
-        if(required in newMap){
-            return [newMap[required],i]
+    // const newMap = [];
+    // for(let i = 0; i<nums.length; i++){
+    //     const currentNum = nums[i]
+    //     const required = target - currentNum
+    //     if(required in newMap){
+    //         return [newMap[required],i]
+    //     }
+    //     newMap[currentNum] = i
+    // }return []
+    
+    for(let i=0; i<nums.length; i++){
+        for(let j=i+1; j<nums.length; j++){
+            if(nums[i] + nums[j]===target) 
+            return [i,j]
         }
-        newMap[currentNum] = i
-    }return []
+    }
 }
